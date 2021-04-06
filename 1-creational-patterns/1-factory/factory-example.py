6e12110ec3fdfb90578664a1e8cf7ea874ccc915
Yash Arya <yarya@vmware.com>
2021-04-05 18:58:46 -0700

    Factory pattern

--- 1-creational-patterns/1-factory/factory-example.py
+++ 1-creational-patterns/1-factory/factory-example.py
@@ -5,3 +5,32 @@ Use the following scenario for the coding challenge.
 * Your pet shop only sold dogs before. Now you sell cats too. You might sell other pets in the future.
 * Your system should show how each of the pets speak.
 '''
+
+class Dog():
+    def __init__(self, name):
+        self._name = name 
+
+    def speak(self):
+        return "Woof!"
+
+class Cat():
+    def __init__(self, name):
+        self._name = name
+    
+    def speak(self):
+        return "Meow!"
+
+# Factory method
+def get_pet(pet):
+    
+    # Factory has information regarding how to instantiate objects of different types.
+    pets = dict(dog=Dog("Ruffy"), cat=Cat("Kitty"))
+    
+    return pets[pet]
+
+if __name__ == "__main__":
+    dog = get_pet("dog")
+    print(dog.speak())
+
+    cat = get_pet("cat")
+    print(cat.speak())
